6c29aae9fc919d710fca58a71ec2d92b5460cb91
Adedamola Adeleke <adeleke.adedamola@gmail.com>
2018-10-11 15:19:15 +0100

    v.1.0.1
    Created New SpringBoot Project using For Room Occupancy Optimization Tool Code Name (ROOT)

---
+++ src/main/java/com/smarthosting/root/RootServicesApplication.java
@@ -0,0 +1,12 @@
+package com.smarthosting.root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootServicesApplication.class, args);
+	}
+}
